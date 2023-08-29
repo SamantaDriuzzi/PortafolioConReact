6ddeabe226a3af01953113a48eac7337c52373e5
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2023-08-29 10:23:36 -0300

    se inicia la sección de 'home' con una imagen de fondo y animación simulando estrellas o luces

---
+++ portafolio/src/components/home/startAnimation.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../../scss/components/starts.scss";
+
+const StarAnimation = () => {
+  const createStars = () => {
+    const stars = [];
+    const shootingStarDelay = Math.random() * 30 + 5;
+
+    for (let i = 0; i < 40; i++) {
+      const style = {
+        top: `${Math.random() * 100}%`,
+        right: `${Math.random() * 30}%`,
+        animationDelay: `${Math.random() * 8}s`,
+      };
+
+      stars.push(<div key={i} className="star" style={style} />);
+    }
+
+    for (let i = 0; i < 3; i++) {
+      const shootingStarStyle = {
+        top: `${Math.random() * 100}%`,
+        right: `${Math.random() * 30}%`,
+        animationDelay: `${shootingStarDelay}s`,
+      };
+
+      stars.push(
+        <div
+          key="shooting-star"
+          className="star-fugaz"
+          style={shootingStarStyle}
+        />
+      );
+    }
+    return stars;
+  };
+
+  return <div className="star-container">{createStars()}</div>;
+};
+
+export default StarAnimation;
