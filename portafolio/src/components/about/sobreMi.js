3ecd31f1b50d256b72c1178d49d65973713c1e87
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2023-08-31 18:08:13 -0300

    agrego @mixin's para mejor organización de las variables en scss

--- portafolio/src/components/about/sobreMi.js
+++ portafolio/src/components/about/sobreMi.js
@@ -4,15 +4,16 @@ import "../../scss/components/sobreMi.scss";
 const SobreMi = () => {
   return (
     <section className="seccionSobreMi" id="sobreMI">
-      <h3 className="saludo">{dataSobreMi.saludo}</h3>
-      <h1 className="nombre">
-        {dataSobreMi.nombre} {dataSobreMi.apellido}
-      </h1>
       <img
         className="imgPerfil"
         src={dataSobreMi.imagen}
         alt="Foto de perfil"
       />
+      <h3 className="saludo">{dataSobreMi.saludo}</h3>
+      <h1 className="nombre">
+        {dataSobreMi.nombre} {dataSobreMi.apellido}
+      </h1>
+
       <div className="contenedor-descripcion">
         <h3 className="descripcion">{dataSobreMi.descripcion}</h3>
         <h3 className="descripcion">{dataSobreMi.segundaDescripcion}</h3>
