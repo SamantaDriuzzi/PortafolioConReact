2d64fa727cf7f1d262d1582cf3f6cdf131a7ff24
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2023-09-04 19:33:14 -0300

    resolución de conflicto de ramas

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -1,9 +1,7 @@
 import SobreMi from "./componentes/sobreMi/sobreMi";
 import Navbar from "./componentes/barraNavegacion/navbar";
 import ProyectosBanner from "./componentes/proyectos/proyectosBanner";
-
 import Home from "./components/home/home";
-
 export default function App() {
   return (
     <div>
