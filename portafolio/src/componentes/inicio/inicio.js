c27c3325e1f4b5ceb842e3f5e7c8f8959f561bbf
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2023-09-14 20:57:44 -0300

    agrego un SVG 'galaxia' en el inicio de la app

--- portafolio/src/componentes/inicio/inicio.js
+++ portafolio/src/componentes/inicio/inicio.js
@@ -2,12 +2,14 @@ import "../../scss/componentes/inicio/inicio.scss";
 import AnimacionEstrellas from "./animacionEstrellas";
 import BotonLink from "../../bibliotecaUI/botonLink.js";
 import MaquinaEscribir from "../../bibliotecaUI/maquinaEscribir";
+import galaxia from "../../imagenes/Iconos/Galaxia.svg";
 
 const Inicio = () => {
   return (
     <section className="seccionHome">
       <div className="contenedor-maquina-escribir">
-        <MaquinaEscribir texto="Hola! Bienvenidx a parte de mi galaxia..." />
+        <MaquinaEscribir texto="¡Hola! Bienvenidx a parte de mi galaxia..." />
+        <img src={galaxia} alt="SVG Alt Text" className="imagen-galaxia" />
       </div>
       <div className="contenedor-btn-link">
         <BotonLink titulo={"ver CV"} toLink={"url"} />
